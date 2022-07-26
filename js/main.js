ba5b70c4ed660b9d54593f1dc6f4d4e844d921d5
Frorex Studio <81986912+FrorexStudio@users.noreply.github.com>
2022-07-26 03:39:00 +0300

    animation

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
 AOS.init();
 
-// var swiper = new Swiper(".mySwiper", {
-//     scrollbar: {
-//       el: ".swiper-scrollbar",
-//       hide: true,
-//     },
-// });
-
 var swiper = new Swiper(".advisorsSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
-    // loop: true,
     scrollbar: {
-      el: ".swiper-scrollbar",
-      hide: true,
+    el: ".swiper-scrollbar",
+    hide: true,
     },
     breakpoints: {
         0: {
-          slidesPerView: 1,
-          spaceBetween: 30,
+        slidesPerView: 1,
+        spaceBetween: 30,
         },
         768: {
-          slidesPerView: 2,
-          spaceBetween: 30,
+        slidesPerView: 2,
+        spaceBetween: 30,
         },
         900: {
             slidesPerView: 3,
             spaceBetween: 30,
         },
         1024: {
-          slidesPerView: 3,
-          spaceBetween: 30,
+        slidesPerView: 3,
+        spaceBetween: 30,
         },
     },
 });
+
+jQuery(document).on('ready', function($) {
+    "use strict";
+
+    var $page = $('html, body');
+    $('a[href*="#header"], a[href*="#create-better-ukraine"], a[href*="#phases"], a[href*="#approach"], a[href*="#advisors"], a[href*="#partners"], a[href*="#engage"], a[href*="#footer"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+        return false;
+    });
+
+}(jQuery));
