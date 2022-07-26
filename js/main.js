fd89dab8ff6af0c4a3ac01a4f2cad9e3db216817
Frorex Studio <81986912+FrorexStudio@users.noreply.github.com>
2022-07-26 03:27:27 +0300

    swiper

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 AOS.init();
+
+// var swiper = new Swiper(".mySwiper", {
+//     scrollbar: {
+//       el: ".swiper-scrollbar",
+//       hide: true,
+//     },
+// });
+
+var swiper = new Swiper(".advisorsSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // loop: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        900: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+    },
+});
